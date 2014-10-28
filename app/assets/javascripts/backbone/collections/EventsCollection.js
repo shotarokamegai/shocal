f0fdec56ad42a18dc330c6bfa12c85cd8068ce3d
shotarokamegai <shotaro326@gmail.com>
2014-10-28 18:15:09 -0400

    deploy to heroku

--- app/assets/javascripts/backbone/collections/EventsCollection.js
+++
@@ -1,6 +0,0 @@
-var ShoCal = ShoCal || { Models: {}, Collections: {}, Views: {} };
-
-ShoCal.Collections.EventCollection = Backbone.Collection.extend({
-	model: ShoCal.Models.Event,
-	url: '/events'
-});
--- app/assets/javascripts/backbone/collections/FriendsCollection.js
+++
@@ -1,6 +0,0 @@
-var ShoCal = ShoCal || { Models: {}, Collections: {}, Views: {} };
-
-ShoCal.Collections.FriendCollection = Backbone.Collection.extend({
-	model: ShoCal.Models.Friend,
-	url: '/friends'
-});
